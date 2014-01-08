e3b9ab349a71e15deadb1fbd5e60aa0d7de53e73
brendosthoughts <morrison.brendan17@gmail.com>
2014-01-08 10:01:40 -0600

    redo  to server micro templating

---
+++ header.php
@@ -0,0 +1,16 @@
+<html lang="en" class=" js csstransforms3d" style="">
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<link rel="shortcut icon" href="http://ec2-54-226-219-255.compute-1.amazonaws.com/tagging/favicon.ico">
+		<link rel="stylesheet" type="text/css" href="css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="css/component.css">
+		<link rel="stylesheet" href="fonts/css/fontello.css">
+			    <link rel="stylesheet" href="fonts/css/animation.css">
+   		<link rel="stylesheet" type="text/css" href="css/search.css" />
+   		<link rel="stylesheet" type="text/css" href="logo/logoInNav.css">
+  		 <link rel="stylesheet" href="home.css">
+<script src="js/jquery-1.10.2.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+		<script src="js/modernizr.custom.js"></script><style type="text/css"></style>
+	</head>
---
+++ tail.php
@@ -0,0 +1,6 @@
+		<script src="js/classie.js"></script>
+		<script src="js/mlpushmenu.js"></script>
+		<script src="js/uisearch.js"></script>
+		<script src="js/nav-dropdown.js"></script>
+		<script src="js/setup.js"></script>
+		</body></html>
