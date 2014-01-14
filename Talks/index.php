8e4eec1b30855f39cb013c99c8191a5c93b9708e
brendosthoughts <morrison.brendan17@gmail.com>
2014-01-14 15:00:42 -0600

    added debates, talk, lectures pages and some reworking of main-template.css

---
+++ Talks/index.php
@@ -0,0 +1,13 @@
+<?php
+include '../../db.class.php';
+$subTemplate="talks.php";
+$toRoot = "../";
+
+include '../header.php';
+include '../main-template.php';
+include '../tail.php';
+
+print_header($subTemplate, $toRoot);
+print_main_template($subTemplate, $toRoot);
+print_tail($subTemplate, $toRoot);
+?>
---
+++ Talks/talks.php
@@ -0,0 +1,50 @@
+<?php 
+require_once("../../paginator.class.php");
+try{
+	
+    $sql = "SELECT COUNT(*)
+        FROM phpro_tag_types types
+        INNER JOIN phpro_tag_targets targets ON types.tag_type_id=targets.tag_type_id
+        INNER JOIN content c ON targets.tag_target_id = c.tag_target_id
+        WHERE types.tag_type_id=21";
+    $stmt = db::getInstance()->prepare($sql);
+    $stmt->execute();
+    $count = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $tag_pages = new Paginator;
+    $tag_pages->items_total = $count[0]['COUNT(*)'];
+    $tag_pages->mid_range = 9;
+    $tag_pages->paginate();
+    $sql = "SELECT DISTINCT content_id, pub_id, title, src_link, cover_img, src_page, description, series, pub_date, season_num, play_time, ep_num, rating, num_views
+        FROM phpro_tag_types types
+        INNER JOIN phpro_tag_targets targets ON types.tag_type_id=targets.tag_type_id
+        INNER JOIN content c ON targets.tag_target_id = c.tag_target_id
+        WHERE types.tag_type_id=21
+        ORDER BY update_time DESC
+        $tag_pages->limit";
+    $stmt = db::getInstance()->prepare($sql);
+    $stmt->execute();
+    $videos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}catch(Exception $e ){
+echo "<h1>404 ... <br> back away , or the world could stop spinning </h1>";
+print_r($e);
+}
+?>
+
+<h2>Documentaries</h2>
+
+<div class="videos">
+
+<ul>
+<?php foreach($videos as $vid){ 
+?> 
+    <li class="item link">
+	<?php display_video($vid); ?>
+   </li>
+
+<?php 
+}
+?>
+</ul>
+<div id='paginator_btns'></div>;
+
+</div>
