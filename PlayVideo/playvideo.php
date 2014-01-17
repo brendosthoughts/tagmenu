aa2f321e60ed4e6ff13e2732f315bcf2bd770d46
brendosthoughts <morrison.brendan17@gmail.com>
2014-01-17 04:10:28 +0000

    added video play and begining integration of videoJS

--- PlayVideo/playvideo.php
+++ PlayVideo/playvideo.php
@@ -1,53 +1,15 @@
-<?php 
-require_once("../../paginator.class.php");
-try{
-	
-    $sql = "SELECT COUNT(*)
-        FROM phpro_tag_types types
-        INNER JOIN phpro_tag_targets targets ON types.tag_type_id=targets.tag_type_id
-        INNER JOIN content c ON targets.tag_target_id = c.tag_target_id
-        WHERE types.tag_type_id=14";
-    $stmt = db::getInstance()->prepare($sql);
-    $stmt->execute();
-    $count = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    $tag_pages = new Paginator;
-    $tag_pages->items_total = $count[0]['COUNT(*)'];
-    $tag_pages->mid_range = 9;
-    $tag_pages->paginate();
-    $sql = "SELECT *
-FROM content c
-WHERE tag_target_id IN (
-    SELECT DISTINCT tag_target_id
-    FROM phpro_tag_types types
-    INNER JOIN phpro_tag_targets targets ON types.tag_type_id=targets.tag_type_id
-    WHERE types.tag_type_id=14
-)
-ORDER BY update_time DESC
-        $tag_pages->limit";
-    $stmt = db::getInstance()->prepare($sql);
-    $stmt->execute();
-    $videos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-}catch(Exception $e ){
-echo "<h1>404 ... <br> back away , or the world could stop spinning </h1>";
-print_r($e);
-}
-?>
+<div class="video-wrapper">
 
-<h2>Documentaries</h2>
+<h2>Video Titles go in the header </h2>
+<video id="feature_video"
+          class="video-js vjs-default-skin"
+          controls
+          preload="auto"
+          width="700"
+          height="300"
+          poster="https://i1.ytimg.com/vi/DuGpw0-B9-s/default.jpg"
+          data-setup='{"techOrder":["youtube"], "src":"http://www.youtube.com/watch?v=KMui1QZB5yw"}'>
+	</video>';
 
-<div class="videos">
 
-<ul>
-<?php foreach($videos as $vid){ 
-?> 
-    <li class="item link">
-	<?php display_video($vid); ?>
-   </li>
-
-<?php 
-}
-?>
-</ul>
-<?php echo "<div id='paginator_btns' >" . $tag_pages->display_pages() . "</div>";
- ?>
 </div>
